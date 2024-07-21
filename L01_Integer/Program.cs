c19ce664979e1c570b06f9e2c405b5a93bfc865c
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-07-21 10:55:48 +0300

    Lesson-001

---
+++ L01_Integer/Program.cs
@@ -0,0 +1,15 @@
+﻿int number1 = 10; //integer -> tam sayı
+
+Int32 number2 = 10;
+
+UInt32 number3 = 10; //unsigned integer -> işaretsiz tam sayı (0 dahil pozitif sayılar)
+
+short number6 = 10;
+
+Int16 number7 = 10;
+
+long number4 = 10;
+
+Int64 number5 = 10;
+
+Int128 number8 = 10;
