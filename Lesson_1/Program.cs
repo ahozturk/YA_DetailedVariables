0c37b920701620ceefbc4303aeb57161b017dc25
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-07-30 14:16:43 +0300

    Lesson-1 & Lesson-2 Added

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Detailed Variables - Yazılım Academy!");
+
+uint number1 = 10; //Integer -> Tam sayı
+
+UInt32 number2 = 10;
+
+UInt16 number3 = 10;
+
+short number4 = 10;
+
+long number5 = 10;
+
+Int64 number6 = 10;
+
+UInt128 number7 = 10;
---
+++ Lesson_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Detailed Variables - Yazılım Academy!");
+
+//DateTime, TimeSpan, DateTimeOffset
+#region Part-1
+//DateTime dateTime1 = DateTime.Now;
+//Console.WriteLine($"dateTime1: {dateTime1}");
+
+//DateTime dateTime2 = DateTime.UtcNow;
+//Console.WriteLine($"dateTime2: {dateTime2}");
+
+//DateTime dateTime3 = new DateTime(2000, 2, 20, 20, 0, 0);
+//Console.WriteLine($"dateTime3: {dateTime3}");
+//Console.WriteLine($"dateTime3 (TR): {dateTime3.ToString("dd/MM/yyyy HH:mm")}");
+//Console.WriteLine($"dateTime3 (TR): {dateTime3.ToString("g", new CultureInfo("tr-TR"))}");
+#endregion
+
+string data = "20.April.2000 20:00";
+
+DateTime dateTimeFromData = DateTime.ParseExact(
+    data,
+    "dd.MMMM.yyyy HH:mm",
+    CultureInfo.CreateSpecificCulture("en-US"));
+
+Console.WriteLine($"dateTimeFromData: {dateTimeFromData.ToString("D", new CultureInfo("tr-TR"))}");
